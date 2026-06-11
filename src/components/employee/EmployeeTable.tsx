550bb22ec2b91fd88f09d47181a08d4356a05d2a
FladSixone <vladis721161@gmail.com>
2026-06-11 16:22:59 +0700

    feature: employee page

--- src/components/employee/EmployeeTable.tsx
+++ src/components/employee/EmployeeTable.tsx
@@ -23,10 +23,8 @@ interface Order {
   email: string;
   phone: string;
   address: string;
-  job: {
   department: string;
   position: string;
-  }
   action: string[];
 }
 
@@ -42,10 +40,8 @@ const tableData: Order[] = [
   email: "rusdiomaygot@gmail.com",
   phone: "082212345678",
   address: "Shlomo Ibn Gabirol St 68, Tel Aviv-Yafo, Israel",
-  job: {
     department: "Staff",
     position: "HRD",
-  },
   action: ["string"],
   },
 ];
@@ -53,6 +49,7 @@ const tableData: Order[] = [
 export default function Employee() {
   return (
     <div className="overflow-hidden rounded-xl border border-brand-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03] shadow-sm">
+      <div className="max-w-full overflow-x-auto"></div>
       <div className="max-w-full overflow-x-auto">
         <div className="min-w-[1102px]">
 
@@ -83,7 +80,7 @@ export default function Employee() {
                 </TableCell>
                 <TableCell
                   isHeader
-                  className="px-5 py-4 font-semibold text-gray-500 text-start text-theme-sm dark:text-gray-400 uppercase"
+                  className="px-5 py-4 font-semibold text-wrap text-gray-500 text-start text-theme-sm dark:text-gray-400 uppercase "
                 >
                   Alamat
                 </TableCell>
@@ -91,11 +88,17 @@ export default function Employee() {
                   isHeader
                   className="px-5 py-4 font-semibold text-gray-500 text-start text-theme-sm dark:text-gray-400 uppercase"
                 >
-                  Departemen & Posisi
+                  Departemen
                 </TableCell>
                 <TableCell
                   isHeader
                   className="px-5 py-4 font-semibold text-gray-500 text-start text-theme-sm dark:text-gray-400 uppercase"
+                >
+                  Posisi
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-4 font-semibold text-gray-500 text-right text-theme-sm dark:text-gray-400 uppercase"
                 >
                   Action
                 </TableCell>
@@ -124,33 +127,27 @@ export default function Employee() {
                       </div>
                     </div>
                   </TableCell>
-                  <TableCell className="px-5 py-4 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                  <TableCell className="px-5 py-4 text-gray-600 text-start text-theme-sm dark:text-gray-400">
                     {order.email}
                   </TableCell>
-                  <TableCell className="px-5 py-4 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                  <TableCell className="px-5 py-4 text-gray-600 text-start text-theme-sm dark:text-gray-400">
                     {order.phone}
                   </TableCell>
-                  <TableCell className="px-5 py-4 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                  <TableCell className="px-5 py-4 text-gray-600 text-start text-theme-sm dark:text-gray-400">
                     {order.address}
                   </TableCell>
-
-                  <TableCell className="px-5 py-4 sm:px-6 text-start">
-                    <div className="flex items-center gap-3">
-                      <div>
-                        <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
-                          {order.job.department}
-                        </span>
-                        <span className="block text-gray-500 text-theme-sm dark:text-gray-400">
-                          {order.job.position}
-                        </span>
-                      </div>
-                    </div>
+                  <TableCell className="px-5 py-4 text-gray-600 text-start text-theme-sm dark:text-gray-400">
+                    {order.department}
                   </TableCell>
+                  <TableCell className="px-5 py-4 text-gray-600 text-start text-theme-sm dark:text-gray-400">
+                    {order.position}
+                  </TableCell>
+                  
 
                   <TableCell className="px-5 py-4">
-                    <div className="flex items-left justify-left gap-2">
-                    <button className=" text-gray-400 hover:text-gray-600 transition-colors"><PencilIcon className="h-auto w-auto" /></button>
-                    <button className=" text-gray-400 hover:text-red-600 transition-colors"><TrashBinIcon className="h-auto w-auto" /></button>
+                    <div className="flex items-end justify-end gap-2 p-1">
+                    <button className=" text-gray-600 hover:text-yellow-500 transition-colors"><PencilIcon className="h-auto w-auto" /></button>
+                    <button className=" text-gray-600 hover:text-red-600 transition-colors"><TrashBinIcon className="h-auto w-auto" /></button>
                     </div>
                   </TableCell>
     
